6225dc46711a064eabd9942c20b663283b27a4f8
sho0126hiro <sho0126hiro@gmail.com>
2019-01-08 14:13:43 +0900

    add type

---
+++ shiwori_app/redux/actions/type.js
@@ -0,0 +1,17 @@
+// actionのtype一覧
+// action/user_data
+// set
+export const SET_USER_ID = 'SET_USER_ID';
+export const SET_USER_NAME = 'SET_USER_NAME';
+export const SET_USER_EMAIL = 'SET_USER_EMAIL';
+export const SET_USER_PASS = 'SET_USER_PASS';
+// clear
+export const CLEAR_USER_ID = 'CLEAR_USER_ID';
+export const CLEAR_USER_NAME = 'CLEAR_USER_NAME';
+export const CLEAR_USER_EMAIL = 'CLEAR_USER_EMAIL';
+export const CLEAR_USER_PASS = 'CLEAR_USER_PASS';
+
+// action/now_reading
+export const ADD_BOOK = 'ADD_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
