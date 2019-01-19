0437b62a2457be7542fcf8716eac3843e2be930b
sho0126hiro <sho0126hiro@gmail.com>
2019-01-19 22:29:44 +0900

    googleBooksAPIに対するgetで、statusCodeも同時に取得できるようにしました

--- shiwori_app/components/debug/gbapi_test.js
+++ shiwori_app/components/debug/gbapi_test.js
@@ -2,33 +2,29 @@
 // reduxの動作確認用コンポーネント
 import React, {Component} from 'react';
 import { View, Text, Button} from 'react-native';
-import {gbapi_search,gbapi_searchStatus} from '../../api/googleBooks/search';
+import {gbapi_search} from '../../api/googleBooks/search';
 
 // googleBooksAPI check
 export default class gbAPIChecker extends Component {
-  state={data:null,data2:null};
-  async gbapiget(){
-    let res = await gbapi_search("python入門");
-    this.setState({data2:JSON.stringify(res)});
-  }
+  state={data:null,data2:null,data3:null};
 
-  async status(){
-    let status = await gbapi_searchStatus("python入門")
-    this.setState({data:status});
+  async gbapiget(){
+    let res = await gbapi_search("ひらがなはいけるかな？");
+    this.setState({data:res.status_code});
+    this.setState({data2:JSON.stringify(res.body)});
+    this.setState({data3:JSON.stringify(res.body.items[0].volumeInfo.title)});
   }
 
   componentWillMount(){
-    this.status();
+    this.gbapiget();
   }
   render() {
     return (
       <View style={{flex: 1}}>
-      <Button
-					title="キーワード検索結果（タイトル）"
-					onPress={() => this.gbapiget()}
-          />  
+      <Text>「ひらがなはいけるかな？」検索結果（上位1件）</Text>
       <Text>status:{this.state.data}</Text>
-      <Text>title:{this.state.data2}</Text>
+      <Text>title:{this.state.data3}</Text>
+      <Text>body:{this.state.data2}</Text>
       </View>
     )
   }
