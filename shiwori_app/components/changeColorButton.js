bc39260cc4f5bd24bf08ff339294132b161e4452
sho0126hiro <sho0126hiro@gmail.com>
2019-01-29 11:33:21 +0900

    押すと色の変わるボタンのコンポーネントを作りました

---
+++ shiwori_app/components/changeColorButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button,TextInput } from 'react-native';
+
+/**
+ * 押すとボタンの色とタイトルが変わるボタン
+ * style,title,pushed_title,color,pushed_colorを指定してください。
+ */
+class changeColorButton extends React.Component {
+    state = { title :this.props.title,color:this.props.color,pushed:false}
+    _onPress(title,changed_title,color,pushedcolor){
+        if(this.state.pushed){
+            this.setState({pushed:false});
+            this.setState({title:title});
+            this.setState({color:color});
+        }else{
+            this.setState({pushed:true})
+            this.setState({title:changed_title})
+            this.setState({color:pushedcolor});
+        }
+    }
+    render() {
+        let style = this.props.style;
+        if(style==undefined)style ={}
+        let title = this.props.title;
+        let pushed_title = this.props.pushed_title;
+        let color = this.props.color;
+        let pushed_color  = this.props.pushed_color; 
+        return (
+        <View>
+            <Button
+                style = {style}
+                onPress={()=>this._onPress(title,pushed_title,color,pushed_color)}
+                title= {this.state.title}
+                color={this.state.color}
+            />
+        </View>
+        );
+    }
+}
+export default changeColorButton;
--- shiwori_app/screens/home/editScreen.js
+++ shiwori_app/screens/home/editScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import HeaderIcon from '../../components/HeaderIcon';
-
+import ChangeColorButton from '../../components/changeColorButton';
 class EditScreen extends React.Component {
   static navigationOptions = ({navigation}) => ({
     title: '読書記録の編集',
@@ -11,6 +11,13 @@ class EditScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Edit Screen</Text>
+        {/* changeColorButtonのデバッグ用 */}
+        <ChangeColorButton 
+        title='おされてない'
+        pushed_title='おされた'
+        color='#f00'
+        pushed_color='#0f0'
+        />
       </View>
     );
   }
